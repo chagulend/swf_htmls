7ea04cd513c2cf76a122ce30d84ecc2711f962fa
chagulend <chris.auglend@gmail.com>
2019-06-01 14:11:17 +0200

    Adding recursive flag -r.

--- flv.py
+++ flv.py
@@ -4,24 +4,32 @@ import click
 
 
 @click.command()
-@click.option("--path", default=".", type=click.Path())
-@click.option("-c", "--clean", is_flag=True)
-def generate_html(path, clean):
-    if (clean):
-        remove_old_html(path)
+@click.option("--path", default=".", type=click.Path(),
+              help="Target directory")
+@click.option("-c", "--clean", is_flag=True,
+              help="Remove old html files")
+@click.option("-r", "--recursive", is_flag=True, help="Recursive")
+def generate_html(path, clean, recursive):
+    if clean:
+        remove_old_html(path, recursive)
     with open("template.html") as f:
         template = f.read()
-    print(path)
     p = Path(path)
-    for swf in p.glob("**/*.swf"):
+    files = p.glob("*.swf")
+    if recursive:
+        files = p.glob("**/*.swf")
+    for swf in files:
         content = template.replace("template", swf.stem)
         html = swf.with_suffix(".html")
         html.write_text(content)
 
 
-def remove_old_html(path):
+def remove_old_html(path, recursive):
     p = Path(path)
-    for html in p.glob("**/*.html"):
+    files = p.glob("*.html")
+    if recursive:
+        files = p.glob("**/*.html")
+    for html in files:
         if html.name == "template.html":
             continue
         html.unlink()
