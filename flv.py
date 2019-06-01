f49261c206630cbfbeb68fba2cf8818055ca23d7
chagulend <chris.auglend@gmail.com>
2019-06-01 13:59:37 +0200

    Passing path to remove_old_html and generate_html.

--- flv.py
+++ flv.py
@@ -4,22 +4,23 @@ import click
 
 
 @click.command()
-@click.option("--path", default=".")
+@click.option("--path", default=".", type=click.Path())
 @click.option("-c", "--clean", is_flag=True)
 def generate_html(path, clean):
     if (clean):
-        remove_old_html()
+        remove_old_html(path)
     with open("template.html") as f:
         template = f.read()
-    p = Path(".")
+    print(path)
+    p = Path(path)
     for swf in p.glob("**/*.swf"):
         content = template.replace("template", swf.stem)
         html = swf.with_suffix(".html")
         html.write_text(content)
 
 
-def remove_old_html():
-    p = Path(".")
+def remove_old_html(path):
+    p = Path(path)
     for html in p.glob("**/*.html"):
         if html.name == "template.html":
             continue
